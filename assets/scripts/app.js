15daea1e6538068a0d42c25be74815275d262191
Khang Do <khangdohuu.vccb@gmail.com>
2020-08-21 12:33:23 -0400

    173. Controlling the Backdrop

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,8 +3,13 @@ const addMovieModal = document.getElementById('add-modal');
 // const addMovieModal = document.querySelector('#add-modal');
 // const addMovieModal = document.body.children[1];
 
+// Selecting the Backdrop div
 const addBackdrop = document.getElementById('backdrop');
-const cancelButton = document.querySelectorAll('.btn--passive');
+// const addBackdrop = document.body.firstElementChild;
+
+// Selecting the Cancel button in the Modal
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+// const cancelButton = document.querySelectorAll('.btn--passive');
 
 // Selecting the Add Movie button on the header
 const startAddMovieButton = document.querySelector('header button');
@@ -19,4 +24,15 @@ const toggleBackdrop = () => {
     addBackdrop.classList.toggle('visible');
 };
 
+const backdropClickHandler = () => {
+    toggleMovieModal();
+};
+
+const cancelAddMovie = () => {
+    toggleMovieModal();
+};
+
 startAddMovieButton.addEventListener('click', toggleMovieModal);
+addBackdrop.addEventListener('click', backdropClickHandler);
+cancelAddMovieButton.addEventListener('click', cancelAddMovie);
+// cancelButton[0].addEventListener('click', toggleMovieModal);
