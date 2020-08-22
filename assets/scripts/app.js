091034380b2b9cbb24951ed897c581bdb9f56a3b
Khang Do <khangdohuu.vccb@gmail.com>
2020-08-22 17:25:35 -0400

    176. Rendering Movie Items on the Screen

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -22,7 +22,36 @@ const startAddMovieButton = document.querySelector('header button');
 const userInputs = addMovieModal.querySelectorAll('input');
 // const userInputs = addMovieModal.getElementsByTagName('input');
 
+const entryTextSection = document.getElementById('entry-text');
+
+// Storing the movie inputs
 const movies = [];
+
+// Add the movies to the list and clear the block
+const updateUI = () => {
+    if (movies.length === 0) {
+        entryTextSection.style.display = 'block';
+    } else {
+        entryTextSection.style.display = 'none';
+    }
+};
+
+// Rendering Movie items on the screen
+const renderNewMovieElement = (title, imageUrl, rating) => {
+    const newMovieElement = document.createElement('li');
+    newMovieElement.className = 'movie-element';
+    newMovieElement.innerHTML = `
+    <div class="movie-element_image">
+        <img src="${imageUrl}" alt="${title}">        
+    </div>
+    <div class="movie-element_info">
+        <h2>${title}</h2>
+        <p>${rating}/5 stars</p>
+    </div>
+    `;
+    const listRoot = document.getElementById('movie-list');
+    listRoot.append(newMovieElement);
+};
 
 const toggleMovieModal = () => { // function() {}
     addMovieModal.classList.toggle('visible');
@@ -63,6 +92,7 @@ const addMovieHandler = () => {
         alert('Please enter valid values (rating between 1 and 5)!');
     }
 
+    // Adding a new movie to the Movies list
     const newMovie = {
         title: titleValue,
         image: imageUrlValue,
@@ -73,6 +103,8 @@ const addMovieHandler = () => {
     console.log(movies);
     toggleMovieModal();
     clearMovieInput();
+    renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating);
+    updateUI();
 };
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
